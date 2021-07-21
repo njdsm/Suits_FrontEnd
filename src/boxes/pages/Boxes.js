1481dea08170d1bffa0d3148ac293c8ab37d1861
Nathanial Johnson <nathanial.r.johnson@gmail.com>
2021-07-21 16:05:57 -0500

    fixed bug with transition of sidedrawer

--- src/boxes/pages/Boxes.js
+++ src/boxes/pages/Boxes.js
@@ -4,7 +4,10 @@ import BoxList from '../components/BoxList';
 
 const Boxes = () => {
     // Temp right now, once we get backend this will pull all the available boxes to display
-    const BOXES = [{id: 'u1', name: 'Nate', image: 'https://m.media-amazon.com/images/M/MV5BY2ViNDIwNmUtZGQ5Ni00MDYxLWI5YTktNWU1NGRlYWQ1ZDg0XkEyXkFqcGdeQXVyOTAyMDgxODQ@._V1_UY1200_CR166,0,630,1200_AL_.jpg', items: 3}];
+    const BOXES = [
+        {id: 'u1', name: 'Box-Name-1', image: 'https://m.media-amazon.com/images/M/MV5BY2ViNDIwNmUtZGQ5Ni00MDYxLWI5YTktNWU1NGRlYWQ1ZDg0XkEyXkFqcGdeQXVyOTAyMDgxODQ@._V1_UY1200_CR166,0,630,1200_AL_.jpg', items: 3},
+        {id: 'u2', name: 'Box-Name-2', image: 'https://m.media-amazon.com/images/M/MV5BY2ViNDIwNmUtZGQ5Ni00MDYxLWI5YTktNWU1NGRlYWQ1ZDg0XkEyXkFqcGdeQXVyOTAyMDgxODQ@._V1_UY1200_CR166,0,630,1200_AL_.jpg', items: 5},
+    ];
     return <BoxList items={BOXES}/>
 };
 
